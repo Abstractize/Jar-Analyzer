33f1b347f02864a2fa85f717b5dcf6e8fd740290
Abstractize <gababarca@hotmail.com>
2018-06-02 23:03:02 -0600

    Creates the classes Graph for the main jar, without the edges.

--- src/main/java/model/Input.java
+++ src/main/java/model/Input.java
@@ -8,14 +8,15 @@ import java.util.jar.JarFile;
 import java.util.jar.Manifest;
 
 import adt.Graph;
+import adt.GraphNode;
 import adt.List;
 
 
-public class Input extends JarFile{
+public class Input{
 
+	private JarFile main;
 	private String Name;
-	private List<Atribute> Jars = new List<Atribute>();
-	private List<JarEntry> Classes = new List<JarEntry>();
+	private List<JAR> Jars = new List<JAR>();
 	private Graph<JAR> Graph;
 	private Manifest Manifest;
 	private JarEntry Classpath;
@@ -24,27 +25,36 @@ public class Input extends JarFile{
 		this(new File(fileURL));
 	}
 	public Input(File file) throws IOException {
-		super(file);
+		main = new JarFile(file);
+		Graph<JarClass> Classes = new Graph<JarClass>();
 		System.out.println(file.getName());
 		setName(file.getName());
 		//Manifest
-		Manifest = this.getManifest();
+		Manifest = main.getManifest();
+		//Get classes
+		for (Enumeration<JarEntry> e = main.entries(); e.hasMoreElements();){
+			JarEntry evaluate = e.nextElement();
+			if (evaluate.getName().endsWith(".class")){
+				JarClass aux = new JarClass(evaluate.getName().replace(".class",""));
+				System.out.println(aux.getName());
+				Classes.insertarVertice(new GraphNode<JarClass>(aux,aux.getName()),false);
+				//Grafo sin aristas
+			}
+		}
+		this.Jars.add(new JAR(main.getName(),Classes));
+		/*
+		System.out.println(Manifest.getEntries());
 		System.out.println("Main attributes");
 		System.out.println(Manifest.getMainAttributes().size());
-		System.out.println(Manifest.getMainAttributes().values());
+		System.out.println(Manifest.getMainAttributes().values());*/
 		//Classpath
-		Classpath = this.getJarEntry(".classpath");
-		System.out.println(Classpath.getName() +" founded");
+		//Classpath = this.getJarEntry(".classpath");
+		//System.out.println(Classpath.getName() +" founded");
 		//Sets Classpath jars
 
 		//Get classes
-		for (Enumeration<JarEntry> e = this.entries(); e.hasMoreElements();){
-			JarEntry evaluate = e.nextElement();
-			if (evaluate.getName().contains(".class")){
-				Classes.add(evaluate);
-			}
-		}
-		Classes.print();
+
+
 		// TODO Auto-generated constructor stub
 	}
 	public void toGraph(){//Search about jar Entries
@@ -57,17 +67,19 @@ public class Input extends JarFile{
 	public void setName(String name) {
 		Name = name;
 	}
-	public List<Atribute> getEntries() {
-		return Jars;
-	}
-	public void setEntries(List<Atribute> jarEntries) {
-		Jars = jarEntries;
-	}
+
 	public Graph getGraph() {
 		return Graph;
 	}
 	public void setGraph(Graph graph) {
 		Graph = graph;
 	}
-	
+
+	public List<JAR> getJars() {
+		return Jars;
+	}
+
+	public void setJars(List<JAR> jars) {
+		Jars = jars;
+	}
 }
