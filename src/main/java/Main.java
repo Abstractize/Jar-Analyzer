0e41e1a73c56e47d76b6d9771f6625b56abf630d
Steve <stevealv@hotmail.com>
2018-06-03 23:24:00 -0600

    Icon added

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,6 +5,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -13,6 +14,7 @@ public class Main extends Application {
         @Override
         public void start(Stage primaryStage) throws Exception{
             Parent root = FXMLLoader.load(getClass().getResource("/fxml/GUI.fxml"));
+            primaryStage.getIcons().add(new Image("/images/iconJar.png"));
             primaryStage.setTitle("Jar Analyzer");
 
             //test
