587b93970151995139f3ebf5d275cc4c05e954df
Steve <stevealv@hotmail.com>
2018-06-15 01:02:33 -0600

    Progress bar added

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,5 @@
 //package main.java;
 
-import adt.Graph;
-import adt.GraphNode;
-import gui.Controller;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
@@ -18,38 +15,9 @@ public class Main extends Application {
             Parent root = FXMLLoader.load(getClass().getResource("/fxml/GUI.fxml"));
             primaryStage.getIcons().add(new Image("/images/iconJar.png"));
             primaryStage.setTitle("Jar Analyzer");
-
-            //test
-            //Controller controller = new Controller();
-            //controller.openDialog(primaryStage);
-
-
             primaryStage.setScene(new Scene(root, 800, 600));
             primaryStage.show();
 
-            //a graph
-
-//            Graph aGra = new Graph();
-//            GraphNode n1 = new GraphNode(1, "ichi");
-//            GraphNode n2 = new GraphNode(2, "ni");
-//            GraphNode n3 = new GraphNode(3, "san");
-//            GraphNode n4 = new GraphNode(4, "yon");
-//            GraphNode n5 = new GraphNode(5, "go");
-//
-//            aGra.insertarVertice(n1, false);
-//            aGra.insertarVertice(n2, false);
-//            aGra.insertarVertice(n3, false);
-//            aGra.insertarVertice(n4, false);
-//            aGra.insertarVertice(n5, false);
-//
-//            aGra.insertarArista(n1,n2);
-//            aGra.insertarArista(n2,n3);
-//            aGra.insertarArista(n3,n4);
-//            aGra.insertarArista(n1,n5);
-//
-//            System.out.println(aGra.getAristas()+"mas");
-//
-//            System.out.println("som");
         }
 
 
--- src/main/java/gui/Controller.java
+++ src/main/java/gui/Controller.java
@@ -3,6 +3,8 @@ package gui;
 import adt.Graph;
 import adt.GraphNode;
 import adt.List;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Group;
@@ -10,7 +12,11 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.control.ProgressBar;
+import javafx.scene.control.ProgressIndicator;
+import javafx.scene.control.Slider;
 import javafx.scene.image.Image;
+import javafx.scene.layout.HBox;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Line;
 import javafx.scene.shape.StrokeLineCap;
@@ -35,11 +41,29 @@ public class Controller {
 
     private boolean fileBool = true;
 
-    private GraphicsContext gc;
-
 
     @FXML
     private Canvas graphContent;
+    @FXML
+    private HBox progressCanvas;
+
+    private void wT() {
+        final Slider slider = new Slider();
+        slider.setMin(0);
+        slider.setMax(50);
+
+        final ProgressBar pb = new ProgressBar(0);
+        final ProgressIndicator pi = new ProgressIndicator(0);
+
+        slider.valueProperty().addListener(new ChangeListener<Number>() {
+            public void changed(ObservableValue<? extends Number> ov,
+                                Number old_val, Number new_val) {
+                pb.setProgress(new_val.doubleValue()/50);
+                pi.setProgress(new_val.doubleValue()/50);
+            }
+        });
+        progressCanvas.getChildren().addAll(slider,pb,pi);
+    }
 
     /**
      * Singleton for openDialog
@@ -111,214 +135,14 @@ public class Controller {
     }
 
     @FXML
-    private void makeGraphics(){
-
-        Random rand = new Random();
-
-        int randWeight = rand.nextInt(540);
-        int randHeight = rand.nextInt(520);
-
-        GraphicsContext graphicsContext = graphContent.getGraphicsContext2D();
-        graphicsContext.clearRect(0,0,graphContent.getWidth(),graphContent.getHeight());
-        //graphicsContext.fillOval(randWeight,randHeight,30,30);
-
-        //nodo1
-//        graphicsContext.strokeOval(randHeight,randWeight,50,50);
-//        graphicsContext.fillText("21",randHeight+15,randWeight+27);
-//
-//        //nodo2
-//        graphicsContext.strokeOval(randHeight+50,randWeight+200,50,50);
-//        graphicsContext.fillText("19",randHeight+15+50,randWeight+27+200);
-//
-//        //vertice
-//        graphicsContext.strokeLine(randHeight+50-10,randWeight+50, randHeight+50+10,randWeight+200);
-//
-//        //vertcice 2.0
-//        graphicsContext.strokeLine(50, 30, 210, 300);
-//        graphicsContext.fillPolygon(
-//                new double[] {200.0, 210.0, 200.0},
-//                new double[] {25.0, 30.0, 35.0},
-//                3
-//        );
-
-        ///Some aGraph
-        Graph aGra = new Graph();
-        GraphNode n1 = new GraphNode(1, "ichi");
-        GraphNode n2 = new GraphNode(2, "ni");
-        GraphNode n3 = new GraphNode(3, "san");
-        GraphNode n4 = new GraphNode(4, "yon");
-        GraphNode n5 = new GraphNode(5, "go");
-
-        aGra.insertarVertice(n1, false);
-        aGra.insertarVertice(n2, false);
-        aGra.insertarVertice(n3, false);
-        aGra.insertarVertice(n4, false);
-        aGra.insertarVertice(n5, false);
-
-        aGra.insertarArista(n1,n2);
-        aGra.insertarArista(n2,n3);
-        aGra.insertarArista(n3,n4);
-        aGra.insertarArista(n1,n5);
-
-
-        //Set of nodes,
-
-        graphicsContext.strokeOval(50,50,50,50);
-        graphicsContext.fillText(n1.getTag(), 65,75);
-
-        graphicsContext.strokeOval(50+100,50-45,50,50);
-        graphicsContext.fillText(n2.getTag(), 65+100,75-45);
-
-        graphicsContext.strokeOval(50-45,50+100,50,50);
-        graphicsContext.fillText(n3.getTag(), 65-45,75+100);
-
-        graphicsContext.strokeOval(50+100-45,50+100-35,50,50);
-        graphicsContext.fillText(n4.getTag(), 65+100-45,75+100-35);
-
-        graphicsContext.strokeOval(50,50+200,50,50);
-        graphicsContext.fillText(n5.getTag(), 65,75+200);
-
-        //Set of edges,
-            //n3 to n4
-        graphicsContext.strokeLine(100-45,75+100,40+100-45,75+100);
-        graphicsContext.fillPolygon(
-                new double[] {100+40-45,50+100-45,100+40-45},
-                new double[] {170,175,180},
-                3
-        );
-            //n1 to n4
-        graphicsContext.strokeLine(100-25,100,200-25-45-10+4,50+100-10+4);
-        graphicsContext.fillPolygon(
-                new double[] {200-79,200-70,150-45+25},
-                new double[] {150-4,150-8,150},
-                3
-        );
-
-
-        //First Level
-        graphicsContext.strokeOval(50+200,50,50,50);
-        graphicsContext.strokeOval(50+300,50-45,50,50);
-        graphicsContext.strokeOval(50+400,50,50,50);
-
-        //Second Level
-        graphicsContext.strokeOval(50+200-45,50+100,50,50);
-        graphicsContext.strokeOval(50+300-45,50+100,50,50);
-        graphicsContext.strokeOval(50+400-45,50+100,50,50);
-
-        //Third Level
-        graphicsContext.strokeOval(50+100,50+200,50,50);
-        graphicsContext.strokeOval(50+200,50+200,50,50);
-        graphicsContext.strokeOval(50+300,50+200,50,50);
-        graphicsContext.strokeOval(50+400,50+200,50,50);
-
-        //Fourth Level
-        graphicsContext.strokeOval(50-45,50+300,50,50);
-        graphicsContext.strokeOval(50+100-45,50+300,50,50);
-        graphicsContext.strokeOval(50+200-45,50+300,50,50);
-        graphicsContext.strokeOval(50+300-45,50+300,50,50);
-        graphicsContext.strokeOval(50+400-45,50+300,50,50);
-
-        //Fifth Level
-        graphicsContext.strokeOval(50,50+400,50,50);
-        graphicsContext.strokeOval(50+100,50+400+30,50,50);
-        graphicsContext.strokeOval(50+200,50+400,50,50);
-        graphicsContext.strokeOval(50+300,50+400+30,50,50);
-        graphicsContext.strokeOval(50+400,50+400,50,50);
-
-    }
-
-    @FXML
-    private void makeAutomatedGraphics(){
-
-        Graph<Integer> aGra = new Graph<Integer>();
-        GraphNode<Integer> n1 = new GraphNode<Integer>(1, "ichi");
-        GraphNode<Integer> n2 = new GraphNode<Integer>(2, "ni");
-        GraphNode<Integer> n3 = new GraphNode<Integer>(3, "san");
-        GraphNode<Integer> n4 = new GraphNode<Integer>(4, "yon");
-        GraphNode<Integer> n5 = new GraphNode<Integer>(5, "go");
-        GraphNode<Integer> n6 = new GraphNode<Integer>(6, "roku");
-        GraphNode<Integer> n7 = new GraphNode<Integer>(7, "nana");
-        GraphNode<Integer> n8 = new GraphNode<Integer>(8, "hachi");
-        GraphNode<Integer> n9 = new GraphNode<Integer>(9, "kyu");
-        GraphNode<Integer> n10 = new GraphNode<Integer>(10, "ju");
-        GraphNode<Integer> n11 = new GraphNode<Integer>(11, "ju ichi");
-        GraphNode<Integer> n12 = new GraphNode<Integer>(12, "ju ni");
-        GraphNode<Integer> n13 = new GraphNode<Integer>(13, "ju san");
-        GraphNode<Integer> n14 = new GraphNode<Integer>(14, "ju yon");
-        GraphNode<Integer> n15 = new GraphNode<Integer>(14, "ju go");
-        GraphNode<Integer> n16 = new GraphNode<Integer>(16, "ju roku");
-        GraphNode<Integer> n17 = new GraphNode<Integer>(17, "ju nana");
-        GraphNode<Integer> n18 = new GraphNode<Integer>(18, "ju hachi");
-        GraphNode<Integer> n19 = new GraphNode<Integer>(19, "ju kyu");
-        GraphNode<Integer> n20 = new GraphNode<Integer>(20, "ni-ju");
-        GraphNode<Integer> n21 = new GraphNode<Integer>(21, "ni-ju ichi");
-        GraphNode<Integer> n22 = new GraphNode<Integer>(22, "ni-ju ni");
-        GraphNode<Integer> n23 = new GraphNode<Integer>(23, "ni-ju san");
-        GraphNode<Integer> n24 = new GraphNode<Integer>(24, "ni-ju yon");
-        GraphNode<Integer> n25 = new GraphNode<Integer>(25, "ni-ju go");
-
-
-
-        aGra.insertarVertice(n1, false);
-        aGra.insertarVertice(n2, false);
-        aGra.insertarVertice(n3, false);
-        aGra.insertarVertice(n4, false);
-        aGra.insertarVertice(n5, false);
-        aGra.insertarVertice(n6, false);
-
-        aGra.insertarArista(n1,n2);
-        aGra.insertarArista(n2,n3);
-        aGra.insertarArista(n3,n4);
-        aGra.insertarArista(n1,n5);
-
-        int nodesQuantity = aGra.getNumberOfNodes();
-
-        GraphicsContext gc =  graphContent.getGraphicsContext2D();
-        gc.clearRect(0,0,graphContent.getWidth(),graphContent.getHeight());
-
-        System.out.println(nodesQuantity);
-
-        int xPlus = 0;
-        int yPlus = 0;
-
-        for (int i=0;nodesQuantity>=i;i++) {
-
-            if (i < 4) {
-
-                gc.strokeOval(50 + xPlus, 50 + yPlus, 50, 50);
-                gc.fillText("ni", 65,75);
-                xPlus += 100;
-
-            }if(i == 4){
-
-                gc.strokeOval(50 + xPlus, 50 + yPlus, 50, 50);
-                xPlus = -45;
-                yPlus += 100;
-
-            }if(4<i && i<8){
-
-                gc.strokeOval(50 + xPlus, 50 + yPlus, 50, 50);
-                xPlus += 100;
-
-            }if(i == 8){
-
-                xPlus = 0;
-                yPlus += 100;
-                gc.strokeOval(50 + xPlus, 50 + yPlus, 50, 50);
-            }
-        }
-
-
-
-    }
-
-    @FXML
     private void clearCanvas(){
         GraphicsContext gc = graphContent.getGraphicsContext2D();
         gc.clearRect(0,0,graphContent.getWidth(),graphContent.getHeight());
+        wT();
     }
+
     @FXML
-    private void callGraphics(){
+    private void drawGraph(){
         GraphicsContext gc = graphContent.getGraphicsContext2D();
         gc.clearRect(0,0,graphContent.getWidth(),graphContent.getHeight());
         Image img = new Image("file:///C:/temp/graphviz-java-api/out.png");
